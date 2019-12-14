890f1b797a54c90225afab6e46e7ad37309b70b6
thomas dubiel <td1729@hotmail.com>
2019-12-14 22:45:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(set)
   i = 0 
   modified_collection = []
   while i < set.size 
-    modified_collection << yield set[i] 
+    modified_collection << yield(set[i])
     i += 1 
   end 
   modified_collection
